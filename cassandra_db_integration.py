355ca2f51e248e0cb1f35926ebb826501976b463
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2022-03-23 18:45:59 -0600

    Finish Cassandra integration

--- cassandra_db_integration.py
+++ cassandra_db_integration.py
@@ -1,7 +1,21 @@
 """A Cassandra integration for the py-wordle game."""
+import json
+from collections import Counter
+from timeit import default_timer as timer
 
 from cassandra.cluster import Cluster
 from cassandra.query import dict_factory
+from wordle_game import WordleGame, get_words, position_level_score, word_level_score
+
+
+def _insert_helper(values):
+    r = ""
+    for v in values:
+        if isinstance(v, (bool, int)):
+            r += "," + str(v)
+        else:
+            r += ",'" + v + "'"
+    return r[1:]
 
 
 class Cassandra:
@@ -44,7 +58,8 @@ class Cassandra:
     def insert(self, table, data):
         """Insert into the specified table."""
         keys = ", ".join(data.keys())
-        values = ", ".join([f"'{v}'" for v in data.values()])
+        # values = ", ".join([f"'{v}'" for v in data.values()])
+        values = _insert_helper(data.values())
         command = f"INSERT INTO {table} ({keys}) VALUES ({values})"
         print(command)
         self.session.execute(command)
@@ -68,31 +83,111 @@ class Cassandra:
         for r in list(self.session.execute("DESCRIBE tables")):
             print(" ", r["name"])
 
-
-table_name = "test"
-
-schema = {
-    "solution": "text",
-    "method": "text",
-    "guesses": "text",
-    "score": "text",
-    "solved": "text",
-}
-
-game_data = {
-    "solution": "raise",
-    "method": "word_level_score",
-    "guesses": "something",
-    "score": str(3),
-    "solved": str(True),
-}
-
-db = Cassandra()
-db.create_table(table_name, schema)
-db.list_tables()
-db.insert(table_name, game_data)
-print(db.select(table_name, schema.keys()))
+    def drop_table(self, table):
+        """Drop the specified table."""
+        command = f"DROP TABLE IF EXISTS {table};"
+        print(command)
+        self.session.execute(command)
 
 
-# https://github.com/dkoepke/cassandra-python-driver/blob/master/example.py
-# https://docs.datastax.com/en/developer/python-driver/3.25/api/
+def evaluate_solution_methods(db, *args):
+    """Solve all possible puzzles using the provided scoring methods.
+
+    A dictionary representing each resulting attempt to solve the puzzle
+    is stored in the specified Cassandra table.
+
+    Blacklist includes puzzles that generated exceptions. This means the
+    scoring algorithm has bugs and sometimes fails.
+    """
+    for method in [*args]:
+        print(method)
+        all_solutions = get_words("wordlist_solutions.txt")
+        blacklist = []
+        unsolved = []
+        num_guesses = []
+        start = timer()
+        for w in all_solutions:
+            try:
+                game = WordleGame(enable_solver=True, solution=w)
+                result, solved = game.solve(method=method)
+                if not solved:
+                    unsolved.append(w)
+                else:
+                    num_guesses.append(len(result))
+                document = {
+                    "solution": w,
+                    # "method": method.__name__,
+                    # "guesses": result,
+                    "score": len(result),
+                    "solved": solved,
+                }
+                db.insert(method.__name__, document)
+            except Exception as e:
+                print(e)
+                blacklist.append(w)
+        end = timer()
+        print(
+            json.dumps(
+                {
+                    "method": method.__name__,
+                    "blacklist": blacklist,
+                    "blacklist count": len(blacklist),
+                    "unsolved": unsolved,
+                    "unsolved count": len(unsolved),
+                    "average score": sum(num_guesses) / len(num_guesses),
+                    "elapsed time": end - start,
+                },
+                indent=4,
+            )
+        )
+
+
+def print_average_score(db, table):
+    """Print the average score for the provided method."""
+    db.session.execute(f"CREATE INDEX IF NOT EXISTS ON {table}(method);")
+    # query = f"SELECT AVG(score) FROM {table};"
+    query = f"SELECT score FROM {table};"
+    avg = [i["score"] for i in list(db.session.execute(query))]
+    avg = sum(avg) / len(avg)
+    print(f"   Average  : {avg}")
+
+
+def print_score_distribution(db, table):
+    """Print the distribution of scores for the provided method."""
+    db.session.execute(f"CREATE INDEX IF NOT EXISTS ON {table}(method);")
+    query = f"SELECT score FROM {table};"
+    avg = [i["score"] for i in list(db.session.execute(query))]
+    for k, v in sorted(dict(Counter(avg)).items()):
+        print(f"   {k} guesses: {v}")
+
+
+def print_failed_solutions(db, table):
+    """Print the number of unsolved puzzles and the target solution."""
+    db.session.execute(f"CREATE INDEX IF NOT EXISTS ON {table}(solved);")
+    query = f"SELECT solution FROM {table} WHERE solved = false ALLOW FILTERING;"
+    words = [i["solution"] for i in list(db.session.execute(query))]
+    print(f"Failed to solve {len(words)} puzzles:")
+    print(words)
+
+
+if __name__ == "__main__":
+
+    schema = {
+        "solution": "text",
+        "method": "text",
+        "guesses": "text",
+        "score": "int",
+        "solved": "boolean",
+    }
+    cassandra_db = Cassandra()
+    # cassandra_db.list_tables()
+
+    for m in (word_level_score, position_level_score):
+        m_name = m.__name__
+        # cassandra_db.drop_table(m_name)
+        # cassandra_db.create_table(m_name, schema)
+        # evaluate_solution_methods(cassandra_db, m)
+        print("\n", "-" * 10, f"{m_name}", "-" * 10)
+        print_average_score(cassandra_db, m_name)
+        print_score_distribution(cassandra_db, m_name)
+        print_failed_solutions(cassandra_db, m_name)
